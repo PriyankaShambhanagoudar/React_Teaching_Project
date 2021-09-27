4397becb26f2f373d94ccc3adc3167f280b82b1a
React_Teaching_Project <priyassss789@gmail.com>
2021-09-27 15:18:45 +0530

    working with sort and filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,51 +1,48 @@
 import React, { useState } from "react";
 
-import ExpenseItem from './Expenseltem';
-import "./Expenses.css"
-import Card from '../UI/Card'
+import ExpenseItem from "./Expenseltem";
+import "./Expenses.css";
+import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
-    const [filteredYear, setFilterYear] = useState('2020');
+  const [filteredYear, setFilterYear] = useState("2020");
 
-    const filterChangeHandler = selectedYear => {
-        setFilterYear(selectedYear)
-    }
+  const filterChangeHandler = (selectedYear) => {
+    setFilterYear(selectedYear);
+  };
 
-    // const filteredExpenses =props.items.filter(expense=>{
-    //     return expense.date.getFullYear().toString() === filteredYear;
-    // })
-    const sortByDesecending = props.items.sort((a,b)=>{
-        
-        return new Date(a.date.toLocaleString()).getTime() - new Date(b.date.toLocaleString()).getTime();
-    
-    });
-
-     const filteredExpenses = sortByDesecending.filter(expense=>{
-         return expense.date.getFullYear().toString() === filteredYear;
-     })
-
-
-    console.log(props.items);
-console.log(sortByDesecending);
-
-
+  // const filteredExpenses =props.items.filter(expense=>{
+  //     return expense.date.getFullYear().toString() === filteredYear;
+  // })
+  const filteredExpenses = props.items.sort((a, b) => {
     return (
-        <div>
-            <Card className="expenses">
-                <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+      new Date(a.date.toLocaleString()).getTime() -
+      new Date(b.date.toLocaleString()).getTime()
+    ) 
+  }).filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
 
-                {filteredExpenses.map((expense) => (
-                    <ExpenseItem 
-                    key={expense.id}
-                        title={expense.title}
-                        amount={expense.amount}
-                        date={expense.date}
-                    />
-                ))}
-            </Card>
-        </div>
-    );
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+
+        {filteredExpenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
+    </div>
+  );
 };
 export default Expenses;
-
