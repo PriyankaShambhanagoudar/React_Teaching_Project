2966feb821efe83b2c04bb21ee2fe37c61f7957c
React_Teaching_Project <priyassss789@gmail.com>
2021-09-28 13:02:19 +0530

    Adding chart

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -3,12 +3,19 @@ import React from "react";
 import "./Chart.css";
 import ChartBar from "./ChartBar";
 
-
-const Chart = props =>{
-    return <div className = "chart">
-        {props.dataPoints.map(dataPoint => <ChartBar  value = {dataPoint.value} maxValue = {null} label = {dataPoint.label}/>)}
-        <ChartBar></ChartBar>
+const Chart = (props) => {
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar
+        key = {dataPoint.label}
+          value={dataPoint.value}
+          maxValue={null}
+          label={dataPoint.label}
+        />
+      ))}
+      <ChartBar></ChartBar>
     </div>
-
-}
+  );
+};
 export default Chart;
