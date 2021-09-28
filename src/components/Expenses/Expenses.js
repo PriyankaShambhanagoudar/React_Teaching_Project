69909c60d441c716afaaf3964e2e48979d0074d7
React_Teaching_Project <priyassss789@gmail.com>
2021-09-28 10:49:51 +0530

    OutPut Conditional Return Statments

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -15,15 +15,28 @@ const Expenses = (props) => {
   // const filteredExpenses =props.items.filter(expense=>{
   //     return expense.date.getFullYear().toString() === filteredYear;
   // })
-  const filteredExpenses = props.items.sort((a, b) => {
-    return (
-      new Date(a.date.toLocaleString()).getTime() -
-      new Date(b.date.toLocaleString()).getTime()
-    ) 
-  }).filter((expense) => {
-    return expense.date.getFullYear().toString() === filteredYear;
-  });
+  const filteredExpenses = props.items
+    .sort(
+      (a, b) =>
+        new Date(a.date.toLocaleString()).getTime() -
+        new Date(b.date.toLocaleString()).getTime()
+    )
+    .filter(
+      (expense) => expense.date.getFullYear().toString() === filteredYear
+    );
 
+  let expenseContent = <p>No Expenses Found.</p>;
+
+  if (filteredExpenses.length > 0) {
+    expenseContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
 
   return (
     <div>
@@ -32,15 +45,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {expenseContent}
       </Card>
     </div>
   );
